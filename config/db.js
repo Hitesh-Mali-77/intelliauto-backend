e47a8017b135a47c204837c8f0eeb8b1892cfff2
Hitesh Mali <hvm7726@gmail.com>
2026-05-27 14:50:35 +0530

    Updated db.js with environment variables

--- config/db.js
+++ config/db.js
@@ -32,14 +32,32 @@
 //   }
 // });
 // ==============================================================
+// import mysql from "mysql";
+
+// export const connection = mysql.createConnection({
+//   host: "mysql.railway.internal",
+//   port: "3306",
+//   user: "root",
+//   password: "PYTBqiQZMsnvjCOxJUqobrancfUyVuwM",
+//   database: "railway",
+// });
+
+// connection.connect((error) => {
+//   if (error) {
+//     console.log("MySQL Connection Error:", error.message);
+//   } else {
+//     console.log("Connected to MySQL !!");
+//   }
+// });
+
 import mysql from "mysql";
 
 export const connection = mysql.createConnection({
-  host: "mysql.railway.internal",
-  port: "3306",
-  user: "root",
-  password: "PYTBqiQZMsnvjCOxJUqobrancfUyVuwM",
-  database: "railway",
+  host: process.env.MYSQLHOST || "mysql.railway.internal",
+  port: process.env.MYSQLPORT || "3306",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "PYTBqiQZMsnvjCOxJUqobrancfUyVuwM",
+  database: process.env.MYSQLDATABASE || "railway",
 });
 
 connection.connect((error) => {
