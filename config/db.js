98cf23c9632fd95fb7dfdc5d8c2457dcccbea2f4
Hitesh Mali <hvm7726@gmail.com>
2026-05-27 14:30:41 +0530

    Force add config/db.js

---
+++ config/db.js
@@ -0,0 +1,51 @@
+// import mysql from "mysql";
+
+// export const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: "3306",
+//   user: "root",
+//   password: "197726",
+//   database: "automobiles",
+// });
+
+// connection.connect((error, result) => {
+//   if (result) {
+//     console.log("Connected to mySQL !!");
+//   }
+// });
+// =================================================================
+// import mysql from "mysql";
+
+// export const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: "3306",
+//   user: "root",
+//   password: "root",
+//   database: "automobiles",
+// });
+
+// connection.connect((error) => {
+//   if (error) {
+//     console.log("MySQL Connection Error:", error.message);
+//   } else {
+//     console.log("Connected to MySQL !!");
+//   }
+// });
+// ==============================================================
+import mysql from "mysql";
+
+export const connection = mysql.createConnection({
+  host: "mysql.railway.internal",
+  port: "3306",
+  user: "root",
+  password: "PYTBqiQZMsnvjCOxJUqobrancfUyVuwM",
+  database: "railway",
+});
+
+connection.connect((error) => {
+  if (error) {
+    console.log("MySQL Connection Error:", error.message);
+  } else {
+    console.log("Connected to MySQL !!");
+  }
+});
