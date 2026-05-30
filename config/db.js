f7333c109026ab1d1e5c5e44d01d850fccd3b52f
Hitesh Mali <hvm7726@gmail.com>
2026-05-30 10:54:06 +0530

    fix: use connection pool

--- config/db.js
+++ config/db.js
@@ -1,69 +1,23 @@
-// import mysql from "mysql";
-
-// export const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: "3306",
-//   user: "root",
-//   password: "197726",
-//   database: "automobiles",
-// });
-
-// connection.connect((error, result) => {
-//   if (result) {
-//     console.log("Connected to mySQL !!");
-//   }
-// });
-// =================================================================
-// import mysql from "mysql";
-
-// export const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: "3306",
-//   user: "root",
-//   password: "root",
-//   database: "automobiles",
-// });
-
-// connection.connect((error) => {
-//   if (error) {
-//     console.log("MySQL Connection Error:", error.message);
-//   } else {
-//     console.log("Connected to MySQL !!");
-//   }
-// });
-// ==============================================================
-// import mysql from "mysql";
-
-// export const connection = mysql.createConnection({
-//   host: "mysql.railway.internal",
-//   port: "3306",
-//   user: "root",
-//   password: "PYTBqiQZMsnvjCOxJUqobrancfUyVuwM",
-//   database: "railway",
-// });
-
-// connection.connect((error) => {
-//   if (error) {
-//     console.log("MySQL Connection Error:", error.message);
-//   } else {
-//     console.log("Connected to MySQL !!");
-//   }
-// });
-
 import mysql from "mysql2";
 
-export const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQLHOST || "mysql.railway.internal",
   port: process.env.MYSQLPORT || "3306",
   user: process.env.MYSQLUSER || "root",
   password: process.env.MYSQLPASSWORD || "PYTBqiQZMsnvjCOxJUqobrancfUyVuwM",
   database: process.env.MYSQLDATABASE || "railway",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((error) => {
-  if (error) {
-    console.log("MySQL Connection Error:", error.message);
+pool.getConnection((err, conn) => {
+  if (err) {
+    console.log("MySQL Connection Error:", err.message);
   } else {
     console.log("Connected to MySQL !!");
+    conn.release();
   }
 });
+
+export { pool as connection };
